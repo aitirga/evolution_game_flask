cd3cdd08093b897f2bf6d83ffb0d3df4705f4f9f
aitirga <aitirga@gmail.com>
2021-03-13 21:17:01 +0100

    bugfix: try fixing issue with heroku

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
 @app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route('/static/<path:path>')
-def send_static(path):
-    return send_from_directory('static', path)
+# @app.route('/static/<path:path>')
+# def send_static(path):
+#     return send_from_directory('static', path)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
